b7afd17feafa9057b3e8e7d5c2c616145feecf52
Juraj Matuš <matus.juraj@yandex.com>
2016-03-30 16:57:58 +0200

    Docker files cleanup

--- logging/start_logging.sh
+++ logging/start_logging.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 source ../env_var.rc
-sed -i "s/NODE1_IP=.*/NODE1_IP=${NODE1_IP}/g" ./Dockerfile
-sed -i "s/NODE2_IP=.*/NODE2_IP=${NODE2_IP}/g" ./Dockerfile
-sed -i "s/RSYSLOG_IP=.*/RSYSLOG_IP=${RSYSLOG_IP}/g" ./Dockerfile
 docker-compose build
 #docker-compose up &> /tmp/logging.log &
 docker-compose up
