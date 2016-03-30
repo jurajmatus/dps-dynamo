008e9d3ebd021a85e6311368543701b66f44be05
Juraj Matuš <matus.juraj@yandex.com>
2016-03-30 17:47:36 +0200

    Service discovery cleanup
    Moving of variables from build time to run time

---
+++ service-discovery/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+agent -server -bootstrap -node="${HOSTNAME}-consul-server" -config-dir="/etc/consul.d" -ui-dir="/ui" -data-dir="/data" -advertise="${HOST_IP}" -client="${HOST_IP}" -join="${NODE1_IP}" -join="${NODE2_IP}"
