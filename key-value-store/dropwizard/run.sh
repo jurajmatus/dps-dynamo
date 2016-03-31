74037f3dbeb6a54cf3534afe69f85abb6818ce94
jurajmatus <matus.juraj@yandex.com>
2016-03-31 11:08:23 +0200

    Key value store cleanup

--- key-value-store/dropwizard/run.sh
+++ key-value-store/dropwizard/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo "${LOGGING_IP} logging" >> /etc/hosts
+
+if [ -n "$LOGGING_IP" ] ; then
+   if [ "$(getent hosts logging) | wc -l" -eq 0 ] ; then
+      echo "$LOGGING_IP logging" >> /etc/hosts
+   fi
+fi
+
 /usr/bin/java -jar /usr/bin/app.jar server /etc/conf.yaml
--- key-value-store/rsyslog/init-rsyslog.sh
+++ key-value-store/rsyslog/init-rsyslog.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-RSYSLOG_HOST="$RSYSLOG_HOST"
-if [ -z "$RSYSLOG_HOST" ] ; then
-   RSYSLOG_HOST="logging"
-fi
 
-if [ "$RSYSLOG_IP" ] ; then
-   RSYSLOG_HOST="$RSYSLOG_IP"
+if [ -n "$LOGGING_IP" ] ; then
+   if [ -z "$(getent hosts logging)" ] ; then
+      echo "$LOGGING_IP logging" >> /etc/hosts
+   fi
 fi
 
 killall rsyslogd
-sed -i '58s/.*/*.* @'$RSYSLOG_HOST'/' /etc/rsyslog.conf
 /usr/sbin/rsyslogd -n
