473f7e907551ec754ce68fa7f232e32b7dcb6162
Juraj Matuš <matus.juraj@yandex.com>
2016-03-31 14:44:46 +0200

    Docker machine - attempt 4

--- docker-machine/create-machines.sh
+++ docker-machine/create-machines.sh
@@ -7,4 +7,6 @@ docker-machine create\
  -d virtualbox\
  --engine-opt="cluster-store=consul://$(docker-machine ip master):8500"\
  --engine-opt="cluster-advertise=eth1:2375"\
+ --engine-opt="bip=192.168.101.1/24"\
+ --engine-opt="fixed-cidr=192.168.101.0/24"\
  slave
--- docker-machine/run-machine.sh
+++ docker-machine/run-machine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROJECT_DIR=$(dirname "$0")
-pushd $PROJECT_DIR/..
+pushd $PROJECT_DIR
 
 if [ -z "$1" ] ; then
    echo "Usage: run-machine.sh master|slave"
@@ -24,21 +24,12 @@ if [ "$1" == "master" ] ; then
    SERVICES="service-discovery proxy-loadbalancer logging key-value-store"
    IP="$(docker-machine ip master)"
    eval "$(docker-machine env master)"
+   docker-compose -f master.yml up
 elif [ "$1" == "slave" ] ; then
    SERVICES="key-value-store"
    IP="$(docker-machine ip slave)"
    eval "$(docker-machine env master)"
+   docker-compose -f slave-yml scale key-value-store=2
+else
+   echo "Unsuported machine $1"
 fi
-
-echo "Started in virtual machine with IP: $IP"
-
-for SERVICE in $SERVICES
-do
-   pushd $SERVICE
-   find . -iname '*build*' -executable | xargs -d '\n' -n 1 --no-run-if-empty sh
-   docker-compose build
-   LOGFILE="/var/log/docker-machine-$SERVICE.log"
-   docker-compose up &> $LOGFILE
-   echo "Started $SERVICE logging to $LOGFILE"
-   popd
-done
