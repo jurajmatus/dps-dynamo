d6ece6884a02dc7430a00bccf1a9d09e0101f09b
Juraj Matuš <matus.juraj@yandex.com>
2016-04-06 09:10:09 +0200

    Environment variables in config files

--- docker-machine/reconfigure-machines.sh
+++ docker-machine/reconfigure-machines.sh
@@ -6,9 +6,12 @@ source ./common.sh
 
 MACHINES_DIR=~/.docker/machine/machines
 
+export MASTER_MACHINE_IP=$(docker-machine ip master)
+export SLAVE_MACHINE_IP=$(docker-machine ip master)
+
 if [ -z $(docker-machine inspect master | grep 'Host does not exist') ] ; then
    docker-machine stop master
-   cp master.json "$MACHINES_DIR/master/config.json"
+   perl -lne '$line=$_; $line =~ s{\$(\w+)}{ exists $ENV{$1} ? $ENV{$1} : q/$/.$1 }ge; $line =~ s{\$\{(\w+)\}}{ exists $ENV{$1} ? $ENV{$1} : q/${/.$1.q/}/ }ge; print "$line";' master.json > "$MACHINES_DIR/master/config.json"
    docker-machine start master
 else
    echo "Master doesn't exist. First you have to create machines."
@@ -16,7 +19,7 @@ fi
 
 if [ -z $(docker-machine inspect slave | grep 'Host does not exist') ] ; then
    docker-machine stop slave
-   cp slave.json "$MACHINES_DIR/slave/config.json"
+   perl -lne '$line=$_; $line =~ s{\$(\w+)}{ exists $ENV{$1} ? $ENV{$1} : q/$/.$1 }ge; $line =~ s{\$\{(\w+)\}}{ exists $ENV{$1} ? $ENV{$1} : q/${/.$1.q/}/ }ge; print "$line";' slave.json > "$MACHINES_DIR/slave/config.json"
    docker-machine start slave
 else
    echo "Master doesn't exist. First you have to create machines."
